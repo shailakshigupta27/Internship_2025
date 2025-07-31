59b5c59dba0df72f11a26bc375be34ba52bdcbba
Shailakshi <gupta.shailakshi27@gmail.com>
2025-07-31 13:54:19 -0500

    Browser_stack_mobile_test

--- features/environment.py
+++ features/environment.py
@@ -36,41 +36,53 @@ def browser_init(context, scenario_name):
 
     #BROWSERSTACK ###
     #Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
-    # bs_user ='shailakshigupta_zAY2tJ'
-    # bs_key = 'jFPVxdiodvfrNkWJr7Xe'
-    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
-    # #
-    # options = Options()
+    bs_user ='shailakshigupta_zAY2tJ'
+    bs_key = 'jFPVxdiodvfrNkWJr7Xe'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    #
+    options = Options()
     # bstack_options = {
     #     "os" : "Windows",
     #    "osVersion" : "11",
     #     'browserName': 'Edge',
     #     'sessionName': scenario_name,
     # }
-    # options.set_capability('bstack:options', bstack_options)
-    # context.driver = webdriver.Remote(command_executor=url, options=options)
+    bstack_options = {
+        'browserName': 'Chrome',
+        "os": "Android",
+        "osVersion": "10.0",
+        "deviceName": "Samsung Galaxy S20 Ultra",
+        "realMobile": "true",
+        "local": "false",
+        'sessionName': scenario_name,
+        "userName": bs_user,
+        "accessKey": bs_key
+    }
+
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
     #
 
 
     #Mobile Emulator
     # from selenium import webdriver
-    mobile_emulation = {"deviceName": "Nexus 5"}
-    options = webdriver.ChromeOptions()
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
-    service = Service(ChromeDriverManager().install())
-    # context.driver = webdriver.Chrome(service=service, options=options)
-    mobile_emulation = {
-        "deviceMetrics": {"width": 360, "height": 640, "pixelRatio": 3.0},
-        "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19",
-        "clientHints": {"platform": "Android", "mobile": True}}
-    chrome_options = Options()
-    chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
-    # context.driver = webdriver.Chrome(chrome_options=chrome_options)
-    context.driver = webdriver.Chrome(options=chrome_options)
+    # mobile_emulation = {"deviceName": "Nexus 5"}
+    # options = webdriver.ChromeOptions()
+    # chrome_options = webdriver.ChromeOptions()
+    # chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+    # service = Service(ChromeDriverManager().install())
+    # # context.driver = webdriver.Chrome(service=service, options=options)
+    # mobile_emulation = {
+    #     "deviceMetrics": {"width": 360, "height": 640, "pixelRatio": 3.0},
+    #     "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19",
+    #     "clientHints": {"platform": "Android", "mobile": True}}
+    # chrome_options = Options()
+    # chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+    # # context.driver = webdriver.Chrome(chrome_options=chrome_options)
+    # context.driver = webdriver.Chrome(options=chrome_options)
 
 
-    context.driver.maximize_window()
+    # context.driver.maximize_window()
     context.driver.implicitly_wait(4)
     context.driver.wait = WebDriverWait(context.driver, timeout=10)
     context.app = Application(context.driver)
